e6fb500fb6b62cf2d011f9ab48e06ec7c65e3e75
Daniel Krezelok <daniel.krezelok@tapmates.com>
2014-07-04 11:57:32 +0200

    Added beacons support

--- FBRelations/FBUserDetailViewController.m
+++ FBRelations/FBUserDetailViewController.m
@@ -92,6 +92,7 @@
   [FBAPI loadUserInfoWithId:_user.userId completetionBlock:^( FBUser *user ) {
     _user = user;
     [self.userDetailView setUser:_user];
+    [[FBBeaconManager sharedInstance] setUser:user];
   } failureBlock:^( NSError *error ) {
     NSLog( @"%@", error );
   }];
