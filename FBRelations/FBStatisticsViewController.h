58e28f1643313ae1551d72a23ed5b3f75b55d6c1
vojtaBelovsky <vojta.belovsky@seznam.cz>
2014-07-11 10:45:36 +0200

    meeting

---
+++ FBRelations/FBStatisticsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBStatisticsViewController.h
+//  FBRelations
+//
+//  Created by Vojtech Belovsky on 08/07/14.
+//  Copyright (c) 2014 Daniel Krezelok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBStatisticsViewController : UIViewController<UITableViewDelegate>
+
+@end
